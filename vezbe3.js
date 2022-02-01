8b7a0af7917d4e6b48ac7a26303a06295d20e8c8
Natasa-Zaric <86966806+Natasa-Zaric@users.noreply.github.com>
2022-02-01 19:54:34 +0100

    Update vezbe3.js

--- vezbe3.js
+++ vezbe3.js
@@ -1,5 +1,5 @@
 AOS.init({
-        offset: 400, // offset (in px) from the original trigger point
-        delay: 0, // values from 0 to 3000, with step 50ms
-        duration: 1000 // values from 0 to 3000, with step 50ms
+        offset: 400, 
+        delay: 0, 
+        duration: 1000 
       });
